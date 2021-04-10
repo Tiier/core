bb32e5326bbd6dd9666d5ae70deb1658e61acc8d
dhershman1 <dustinh17@gmail.com>
2021-04-10 15:51:51 -0400

    Re configure to bare bones

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  title: 'Test'
+}
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/200.png">
+    <Basic msg="A Basic Home Page!"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Basic from '@/components/Basic.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Basic
   }
 }
 </script>
